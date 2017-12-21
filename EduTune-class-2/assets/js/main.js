d4227266547eebc04c57a127faad5db2755f52ee
parthatalukder <33701251+parthatalukder@users.noreply.github.com>
2017-12-22 02:46:12 +0600

    Add files via upload

---
+++ EduTune-class-2/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function($){
+
+		$(".video_btn").modalVideo();
+		$(".video_btn").on('click', function() {
+			return false;
+		});
+
+
+
+		
+
+
+	});
+
+
+	jQuery(window).load(function(){
+
+		
+	});
+
+
+}(jQuery));	
